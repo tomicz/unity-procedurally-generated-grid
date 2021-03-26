bbe1927efe93700c853a5c945bd75b5b902bd38a
Darko Tomic <tomicz@192.168.0.104>
2021-03-26 08:09:09 +0100

    Refactored: OptimizedGrid class has been refactored and method names simplifed. Also, added better description to methods

--- Assets/Tomicz/Examples/GridGenerator.cs
+++ Assets/Tomicz/Examples/GridGenerator.cs
@@ -17,6 +17,7 @@ public class GridGenerator : MonoBehaviour
     [SerializeField] private float nodeHeight = 1f;
 
     private Mesh mesh = null;
+
     private OptimizedGrid _grid;
 
     private void Awake()
@@ -30,6 +31,11 @@ public class GridGenerator : MonoBehaviour
         _grid = new OptimizedGrid(gridWidth, gridHeight, nodeWidth, nodeHeight, spacing);
         _grid.GenerateGrid();
 
+        LoadMeshData();
+    }
+
+    private void LoadMeshData()
+    {
         mesh.vertices = _grid.Vertices;
         mesh.triangles = _grid.Triangles;
 
---
+++ Assets/Tomicz/Scripts/Grid/Node.cs
@@ -0,0 +1,14 @@
+namespace Tomicz.Grid
+{
+    public struct Node
+    {
+        public int x;
+        public int y;
+
+        public Node(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
