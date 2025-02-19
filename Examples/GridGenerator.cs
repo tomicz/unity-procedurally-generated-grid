7005a5e674035553dc7b762e198fb760ab96f593
Tomicz Engineering LLC <tomiczdarko@gmail.com>
2025-02-19 11:22:06 +0100

    Added: Change node vertext color at a specific coordinate

--- Examples/GridGenerator.cs
+++ Examples/GridGenerator.cs
@@ -6,6 +6,8 @@ using TOMICZ.Grid;
 [RequireComponent(typeof(MeshRenderer))]
 public class GridGenerator : MonoBehaviour
 {
+    public OptimizedGrid Grid => _grid;
+
     [SerializeField] private int gridWidth;
     [SerializeField] private int gridHeight;
     [SerializeField] private float spacing = .1f;
@@ -15,6 +17,15 @@ public class GridGenerator : MonoBehaviour
 
     private Mesh _mesh;
     private OptimizedGrid _grid;
+
+    public void SetNodeColor(int x, int y, Color color)
+    {
+        if (_grid != null)
+        {
+            _grid.SetNodeColor(x, y, color);
+            _grid.LoadMeshData(_mesh);
+        }
+    }
 
     private void Awake()
     {
@@ -30,7 +41,7 @@ public class GridGenerator : MonoBehaviour
             _mesh = meshFilter.sharedMesh;
         }
 
-        Material gridMaterial = new Material(Shader.Find("Unlit/Color"));
+        Material gridMaterial = new Material(Shader.Find("Custom/VertexColor"));
         Renderer renderer = GetComponent<Renderer>();
 
         if(renderer != null)
--- Runtime/OptimizedGrid.cs
+++ Runtime/OptimizedGrid.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using TOMICZ.Grid.GeometryShapes;
 using UnityEngine;
 
 namespace TOMICZ.Grid
@@ -7,24 +6,19 @@ namespace TOMICZ.Grid
     public class OptimizedGrid
     {
         public int gridWidth;
-
         public int gridHeight;
-
         public float nodeWidth;
-
         public float nodeHeight;
-
         public float spacing;
 
         public Vector3[] Vertices => _verticesList.ToArray();
-
         public int[] Triangles => _trianglesList.ToArray();
-
-        private const int AngleOffset = 4;
+        public Color[] Colors => _colorsList.ToArray();
 
         private List<Vector3> _verticesList;
-
         private List<int> _trianglesList;
+        private List<Color> _colorsList;
+        private Color _defaultColor = Color.white;
 
         public OptimizedGrid(int gridWidth, int gridHeight, float nodeWidth, float nodeHeight, float spacing)
         {
@@ -37,16 +31,29 @@ namespace TOMICZ.Grid
 
         public void GenerateGrid(bool isHorizontal = false)
         {
+            InitializeLists();
+            CalculateGridDimensions(out float startX, out float startY);
+            GenerateNodes(startX, startY, isHorizontal);
+        }
+
+        private void InitializeLists()
+        {
             _verticesList = new List<Vector3>();
             _trianglesList = new List<int>();
+            _colorsList = new List<Color>();
+        }
 
-            int vertexIndex = 0;
-            int triangleIndex = 0;
-
+        private void CalculateGridDimensions(out float startX, out float startY)
+        {
             float totalWidth = gridWidth * (nodeWidth + spacing) - spacing;
             float totalHeight = gridHeight * (nodeHeight + spacing) - spacing;
-            float startX = -totalWidth / 2f;
-            float startY = -totalHeight / 2f;
+            startX = -totalWidth / 2f;
+            startY = -totalHeight / 2f;
+        }
+
+        private void GenerateNodes(float startX, float startY, bool isHorizontal)
+        {
+            int vertexIndex = 0;
 
             for (int y = 0; y < gridHeight; y++)
             {
@@ -55,67 +62,74 @@ namespace TOMICZ.Grid
                     float xPos = startX + x * (nodeWidth + spacing);
                     float yPos = startY + y * (nodeHeight + spacing);
 
-                    if (isHorizontal)
-                    {
-                        _verticesList.Add(new Vector3(xPos, 0, yPos));
-                        _verticesList.Add(new Vector3(xPos + nodeWidth, 0, yPos));
-                        _verticesList.Add(new Vector3(xPos, 0, yPos + nodeHeight));
-                        _verticesList.Add(new Vector3(xPos + nodeWidth, 0, yPos + nodeHeight));
-                    }
-                    else
-                    {
-                        _verticesList.Add(new Vector3(xPos, yPos, 0));
-                        _verticesList.Add(new Vector3(xPos + nodeWidth, yPos, 0));
-                        _verticesList.Add(new Vector3(xPos, yPos + nodeHeight, 0));
-                        _verticesList.Add(new Vector3(xPos + nodeWidth, yPos + nodeHeight, 0));
-                    }
-
-                    _trianglesList.Add(vertexIndex);
-                    _trianglesList.Add(vertexIndex + 2);
-                    _trianglesList.Add(vertexIndex + 1);
-                    _trianglesList.Add(vertexIndex + 2);
-                    _trianglesList.Add(vertexIndex + 3);
-                    _trianglesList.Add(vertexIndex + 1);
+                    AddNodeVertices(xPos, yPos, isHorizontal);
+                    AddNodeColors();
+                    AddNodeTriangles(vertexIndex);
 
                     vertexIndex += 4;
-                    triangleIndex += 6;
                 }
+            }
+        }
+
+        private void AddNodeVertices(float xPos, float yPos, bool isHorizontal)
+        {
+            if (isHorizontal)
+            {
+                _verticesList.Add(new Vector3(xPos, 0, yPos));
+                _verticesList.Add(new Vector3(xPos + nodeWidth, 0, yPos));
+                _verticesList.Add(new Vector3(xPos, 0, yPos + nodeHeight));
+                _verticesList.Add(new Vector3(xPos + nodeWidth, 0, yPos + nodeHeight));
+            }
+            else
+            {
+                _verticesList.Add(new Vector3(xPos, yPos, 0));
+                _verticesList.Add(new Vector3(xPos + nodeWidth, yPos, 0));
+                _verticesList.Add(new Vector3(xPos, yPos + nodeHeight, 0));
+                _verticesList.Add(new Vector3(xPos + nodeWidth, yPos + nodeHeight, 0));
+            }
+        }
+
+        private void AddNodeColors()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                _colorsList.Add(_defaultColor);
+            }
+        }
+
+        private void AddNodeTriangles(int vertexIndex)
+        {
+            _trianglesList.Add(vertexIndex);
+            _trianglesList.Add(vertexIndex + 2);
+            _trianglesList.Add(vertexIndex + 1);
+            _trianglesList.Add(vertexIndex + 2);
+            _trianglesList.Add(vertexIndex + 3);
+            _trianglesList.Add(vertexIndex + 1);
+        }
+
+        public void SetNodeColor(int x, int y, Color color)
+        {
+            if (x < 0 || x >= gridWidth || y < 0 || y >= gridHeight)
+                return;
+
+            int vertexIndex = (y * gridWidth + x) * 4;
+            
+            for (int i = 0; i < 4; i++)
+            {
+                _colorsList[vertexIndex + i] = color;
             }
         }
 
         public void LoadMeshData(Mesh mesh)
         {
             if(mesh == null)
-            {
                 return;
-            }
 
             mesh.Clear();
-
             mesh.vertices = _verticesList.ToArray();
             mesh.triangles = _trianglesList.ToArray();
-
+            mesh.colors = _colorsList.ToArray();
             mesh.RecalculateNormals();
         }
-
-        private void InitilizeDataContainers()
-        {
-            _trianglesList = new List<int>();
-            _verticesList = new List<Vector3>();
-        }
-
-        private void SortQuadData(int index, Quad quad)
-        {
-            SortVerticies(quad);
-            SortTriangles(index, quad);
-        }
-
-        private void SortVerticies(Quad quad) => _verticesList.AddRange(quad.GetVerticies());
-
-        private void SortTriangles(int index, Quad quad) => _trianglesList.AddRange(quad.GetTriangles(AngleOffset * index));
-
-        private Quad AddQuad(int x, int y) => new Quad(Position(x), Position(y), nodeWidth, nodeHeight);
-
-        private float Position(float coord) => coord * (nodeWidth + spacing);
     }
 }
