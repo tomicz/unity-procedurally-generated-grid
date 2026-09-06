86511339483885c76ba7d4dcbda0affa715091a4
TOMICZ <tomiczdarko@gmail.com>
2026-09-06 10:09:03 +0200

    Move GridGenerator into the TOMICZ.Grid namespace
    GridGenerator was the only type in the global namespace. Private
    serialized fields now follow the underscore convention used by the rest
    of the code; FormerlySerializedAs keeps existing scene values intact.
    The asmdef also declares the root namespace.

--- Runtime/GridGenerator.cs
+++ Runtime/GridGenerator.cs
@@ -1,76 +1,52 @@
-using UnityEngine;
-using TOMICZ.Grid;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
 
-[ExecuteInEditMode]
-[RequireComponent(typeof(MeshFilter))]
-[RequireComponent(typeof(MeshRenderer))]
-public class GridGenerator : MonoBehaviour
+namespace TOMICZ.Grid
 {
-    public OptimizedGrid Grid => _grid;
-
-    [SerializeField] private int gridWidth;
-    [SerializeField] private int gridHeight;
-    [SerializeField] private float spacing = .1f;
-    [SerializeField] private float nodeWidth = 1f;
-    [SerializeField] private float nodeHeight = 1f;
-    [SerializeField] private bool isHorizontal;
-    [SerializeField] private Material _defaultMaterial;
-
-    private Mesh _mesh;
-    private OptimizedGrid _grid;
-    private List<MeshFilter> _additionalMeshFilters = new();
-
-    public bool IsHorizontal => isHorizontal;
-
-    public void SetNodeColor(int x, int y, Color color)
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(MeshFilter))]
+    [RequireComponent(typeof(MeshRenderer))]
+    public class GridGenerator : MonoBehaviour
     {
-        if (_grid != null)
+        [SerializeField, FormerlySerializedAs("gridWidth")] private int _gridWidth = 10;
+        [SerializeField, FormerlySerializedAs("gridHeight")] private int _gridHeight = 10;
+        [SerializeField, FormerlySerializedAs("spacing")] private float _spacing = .1f;
+        [SerializeField, FormerlySerializedAs("nodeWidth")] private float _nodeWidth = 1f;
+        [SerializeField, FormerlySerializedAs("nodeHeight")] private float _nodeHeight = 1f;
+        [SerializeField, FormerlySerializedAs("isHorizontal")] private bool _isHorizontal;
+        [SerializeField] private Material _defaultMaterial;
+
+        private Mesh _mesh;
+        private OptimizedGrid _grid;
+        private List<MeshFilter> _additionalMeshFilters = new();
+
+        public OptimizedGrid Grid => _grid;
+        public bool IsHorizontal => _isHorizontal;
+
+        public void SetNodeColor(int x, int y, Color color)
         {
-            _grid.SetNodeColor(x, y, color);
-            
-            _grid.LoadMeshData(_mesh);
-            for (int i = 0; i < _additionalMeshFilters.Count; i++)
+            if (_grid != null)
             {
-                if (_additionalMeshFilters[i] != null && i + 1 < _grid.MeshSections.Count)
+                _grid.SetNodeColor(x, y, color);
+
+                _grid.LoadMeshData(_mesh);
+                for (int i = 0; i < _additionalMeshFilters.Count; i++)
                 {
-                    var section = _grid.MeshSections[i + 1];
-                    var sectionMesh = _additionalMeshFilters[i].sharedMesh;
-                    if (sectionMesh != null)
+                    if (_additionalMeshFilters[i] != null && i + 1 < _grid.MeshSections.Count)
                     {
-                        sectionMesh.colors = section.Colors.ToArray();
+                        var section = _grid.MeshSections[i + 1];
+                        var sectionMesh = _additionalMeshFilters[i].sharedMesh;
+                        if (sectionMesh != null)
+                        {
+                            sectionMesh.colors = section.Colors.ToArray();
+                        }
                     }
                 }
             }
         }
-    }
 
-    private void Awake()
-    {
-        var meshFilter = GetComponent<MeshFilter>();
-        if (meshFilter.sharedMesh == null)
-        {
-            _mesh = new Mesh();
-            _mesh.name = "Grid Mesh Section 0";
-            meshFilter.sharedMesh = _mesh;
-        }
-        else
-        {
-            _mesh = meshFilter.sharedMesh;
-        }
-
-        Material gridMaterial = new Material(Shader.Find("Custom/VertexColor"));
-        Renderer renderer = GetComponent<Renderer>();
-
-        if(renderer != null)
-        {
-            renderer.material = gridMaterial;
-        }
-    }
-
-    private void OnValidate()
-    {
-        if (_mesh == null)
+        private void Awake()
         {
             var meshFilter = GetComponent<MeshFilter>();
             if (meshFilter.sharedMesh == null)
@@ -83,81 +59,107 @@ public class GridGenerator : MonoBehaviour
             {
                 _mesh = meshFilter.sharedMesh;
             }
-        }
 
-        gridWidth = Mathf.Max(0, gridWidth);
-        gridHeight = Mathf.Max(0, gridHeight);
+            Material gridMaterial = new Material(Shader.Find("Custom/VertexColor"));
+            Renderer renderer = GetComponent<Renderer>();
 
-        RegenerateGrid();
-    }
-
-    private void RegenerateGrid()
-    {
-        ClearAdditionalMeshes();
-
-        if (_mesh != null)
-        {
-            _mesh.Clear();
-            var meshFilter = GetComponent<MeshFilter>();
-            if (meshFilter != null)
+            if (renderer != null)
             {
-                meshFilter.sharedMesh = _mesh;
+                renderer.material = gridMaterial;
             }
         }
 
-        _grid = new OptimizedGrid(gridWidth, gridHeight, nodeWidth, nodeHeight, spacing);
-        _grid.GenerateGrid(isHorizontal);
-
-        _grid.LoadMeshData(_mesh);
-
-        for (int i = 1; i < _grid.MeshSections.Count; i++)
+        private void OnValidate()
         {
-            var go = new GameObject($"Grid Section {i}");
-            go.transform.SetParent(transform, false);
-            go.transform.localPosition = Vector3.zero;
-            go.transform.localRotation = Quaternion.identity;
-            
-            var mf = go.AddComponent<MeshFilter>();
-            var mr = go.AddComponent<MeshRenderer>();
-            mr.material = GetComponent<Renderer>().sharedMaterial;
-            
-            var sectionMesh = new Mesh();
-            sectionMesh.name = $"Grid Mesh Section {i}";
-            mf.sharedMesh = sectionMesh;
-            
-            var section = _grid.MeshSections[i];
-            sectionMesh.vertices = section.Vertices.ToArray();
-            sectionMesh.triangles = section.Triangles.ToArray();
-            sectionMesh.colors = section.Colors.ToArray();
-            sectionMesh.RecalculateNormals();
-            
-            _additionalMeshFilters.Add(mf);
-        }
-    }
-
-    private void ClearAdditionalMeshes()
-    {
-        foreach (var filter in _additionalMeshFilters)
-        {
-            if (filter != null)
+            if (_mesh == null)
             {
-                if (filter.sharedMesh != null)
+                var meshFilter = GetComponent<MeshFilter>();
+                if (meshFilter.sharedMesh == null)
                 {
-                    if (Application.isPlaying)
-                        Destroy(filter.sharedMesh);
-                    else
-                        DestroyImmediate(filter.sharedMesh);
+                    _mesh = new Mesh();
+                    _mesh.name = "Grid Mesh Section 0";
+                    meshFilter.sharedMesh = _mesh;
                 }
-
-                if (filter.gameObject != null)
+                else
                 {
-                    if (Application.isPlaying)
-                        Destroy(filter.gameObject);
-                    else
-                        DestroyImmediate(filter.gameObject);
+                    _mesh = meshFilter.sharedMesh;
                 }
             }
+
+            _gridWidth = Mathf.Max(0, _gridWidth);
+            _gridHeight = Mathf.Max(0, _gridHeight);
+
+            RegenerateGrid();
         }
-        _additionalMeshFilters.Clear();
+
+        private void RegenerateGrid()
+        {
+            ClearAdditionalMeshes();
+
+            if (_mesh != null)
+            {
+                _mesh.Clear();
+                var meshFilter = GetComponent<MeshFilter>();
+                if (meshFilter != null)
+                {
+                    meshFilter.sharedMesh = _mesh;
+                }
+            }
+
+            _grid = new OptimizedGrid(_gridWidth, _gridHeight, _nodeWidth, _nodeHeight, _spacing);
+            _grid.GenerateGrid(_isHorizontal);
+
+            _grid.LoadMeshData(_mesh);
+
+            for (int i = 1; i < _grid.MeshSections.Count; i++)
+            {
+                var go = new GameObject($"Grid Section {i}");
+                go.transform.SetParent(transform, false);
+                go.transform.localPosition = Vector3.zero;
+                go.transform.localRotation = Quaternion.identity;
+
+                var mf = go.AddComponent<MeshFilter>();
+                var mr = go.AddComponent<MeshRenderer>();
+                mr.material = GetComponent<Renderer>().sharedMaterial;
+
+                var sectionMesh = new Mesh();
+                sectionMesh.name = $"Grid Mesh Section {i}";
+                mf.sharedMesh = sectionMesh;
+
+                var section = _grid.MeshSections[i];
+                sectionMesh.vertices = section.Vertices.ToArray();
+                sectionMesh.triangles = section.Triangles.ToArray();
+                sectionMesh.colors = section.Colors.ToArray();
+                sectionMesh.RecalculateNormals();
+
+                _additionalMeshFilters.Add(mf);
+            }
+        }
+
+        private void ClearAdditionalMeshes()
+        {
+            foreach (var filter in _additionalMeshFilters)
+            {
+                if (filter != null)
+                {
+                    if (filter.sharedMesh != null)
+                    {
+                        if (Application.isPlaying)
+                            Destroy(filter.sharedMesh);
+                        else
+                            DestroyImmediate(filter.sharedMesh);
+                    }
+
+                    if (filter.gameObject != null)
+                    {
+                        if (Application.isPlaying)
+                            Destroy(filter.gameObject);
+                        else
+                            DestroyImmediate(filter.gameObject);
+                    }
+                }
+            }
+            _additionalMeshFilters.Clear();
+        }
     }
 }
